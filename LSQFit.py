c2bd2c471119bf6830d02bb6526d426ad9a59d1b
William Kerr <yun5pc@virginia.edu>
2025-10-20 10:19:57 -0400

    completed exercise for LSQFit.py

--- LSQFit.py
+++ LSQFit.py
@@ -38,19 +38,61 @@ fig.show()
 
 # *** modify and add your code here ***
 nexperiments = 1000  # for example
+nPar = 3
+ndf = npoints-nPar
+par_a = np.zeros(nexperiments)
+par_b = np.zeros(nexperiments)
+par_c = np.zeros(nexperiments)
+chi2_reduced = np.zeros(nexperiments)
 
 
 # perform many least squares fits on different pseudo experiments here
+for i in range(nexperiments):
+    getX(lx)
+    getY(lx, ly, ley)
+
+    weights = 1.0/ley
+
+    A = np.zeros((npoints, nPar))
+    log_lx = np.log(lx)
+    A[:, 0] = 1.0
+    A[:, 1] = log_lx
+    A[:, 2] = log_lx**2
+
+    A_w = A*weights[:, np.newaxis]
+    y_w = (ly*weights).reshape(npoints, 1)
+
+    AT_A_inv = inv(A_w.T @ A_w)
+    AT_y = A_w.T @ y_w
+
+    theta = AT_A_inv @ AT_y
+
+    par_a[i] = theta[0,0]
+    par_b[i] = theta[1,0]
+    par_c[i] = theta[2,0]
+
+    residuals_w = y_w - (A_w @ theta)
+    chi2 = np.sum(residuals_w**2)
+    chi2_reduced[i] = chi2/ndf
 # fill histograms w/ required data
 
-par_a = np.random.rand(1000)   # simple placeholders for making the plot example
-par_b = np.random.rand(1000)   # these need to be filled using results from your fits
-par_c = np.random.rand(1000)
-chi2_reduced = np.random.rand(1000)
+# par_a = np.random.rand(1000)   # simple placeholders for making the plot example
+# par_b = np.random.rand(1000)   # these need to be filled using results from your fits
+# par_c = np.random.rand(1000)
+# chi2_reduced = np.random.rand(1000)
+
+print(f"True values: a={pars[0]}, b={pars[1]}, c={pars[2]}")
+print(f"Fitted 'a': mean = {np.mean(par_a):.4f}, std = {np.std(par_a):.4f}")
+print(f"Fitted 'b': mean = {np.mean(par_b):.4f}, std = {np.std(par_b):.4f}")
+print(f"Fitted 'c': mean = {np.mean(par_c):.4f}, std = {np.std(par_c):.4f}")
+print(f"Reduced Chi^2: mean = {np.mean(chi2_reduced):.4f}, std = {np.std(chi2_reduced):.4f}")
+print(f"(Expected mean for reduced chi^2 is 1)")
+print(f"(Expected std dev for reduced chi^2 is sqrt(2/ndf) = {np.sqrt(2/ndf):.4f})")
 
 fig, axs = plt.subplots(2, 2)
 plt.tight_layout()
 
+bins = 60
 # careful, the automated binning may not be optimal for displaying your results!
 axs[0, 0].hist2d(par_a, par_b)
 axs[0, 0].set_title('Parameter b vs a')
@@ -64,6 +106,7 @@ axs[1, 0].set_title('Parameter c vs b')
 axs[1, 1].hist(chi2_reduced)
 axs[1, 1].set_title('Reduce chi^2 distribution')
 
+fig.savefig("LSQFit_Results.pdf")
 fig.show()
 
 # **************************************
